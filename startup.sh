cba1a6f9b411ccb20d9025039e6ef8e3a4c5962d
Bryt Li <bryt@oriente.com>
2018-03-14 12:46:42 +0700

    update

--- startup.sh
+++ startup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-mkdir conf-mysql-db
-mkdir webapps
-
 cp -f xxl-conf-admin.properties xxl-conf/xxl-conf-admin/src/main/resources/xxl-conf-admin.properties
 
 cd xxl-conf
 mvn clean
 mvn package
-
 cd ..
+
+rm -rf webapps
+mkdir webapps
 cp xxl-conf/xxl-conf-admin/target/xxl-conf-admin-1.4.1-SNAPSHOT.war webapps/ROOT.war
+mkdir conf-mysql-db
 
 docker-compose up
