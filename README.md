160fcb3b95d045593a085753b76837385c6644d5
Bryt Li <bryt@oriente.com>
2018-03-14 12:16:37 +0700

    update

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 #synchronize submodule xxl-conf with xxl's github repo
 git submodule update --init --recursive
 ```
+
+```
+npm start
+npm run stop
+```
--- startup.sh
+++ startup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 mkdir conf-mysql-db
+mkdir webapps
 
 cp -f xxl-conf-admin.properties xxl-conf/xxl-conf-admin/src/main/resources/xxl-conf-admin.properties
-cd xxl-conf
 
+cd xxl-conf
 mvn clean
 mvn package
-mkdir xxl-conf-admin/target/webapps
-cp xxl-conf-admin/target/xxl-conf-admin-1.4.1-SNAPSHOT.war xxl-conf-admin/target/webapps/ROOT.war
 
 cd ..
+cp xxl-conf/xxl-conf-admin/target/xxl-conf-admin-1.4.1-SNAPSHOT.war webapps/ROOT.war
 
 docker-compose up
